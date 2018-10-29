76d53481104ec175d455fe8a3b522cace1437973
Frank Duncan <frank@kank.net>
2018-10-29 07:27:37 -0500

    Import csv2wiki and use in simple case.
    When importing, done as a vcs style package, so that running `pipenv
    install` will update as things are done.  Upload is very simple and
    counts on you basically being able to have run wiki-refresh in macfound
    with zero errors (if the previous statement makes no sense to you, this
    project has moved forward enough that it doesn't need to).

--- wikis/__init__.py
+++ wikis/__init__.py
@@ -2,6 +2,10 @@ from flask import (
     Blueprint, render_template, request
 )
 
+from io import StringIO
+
+import csv2wiki
+
 bp = Blueprint('wikis', __name__, url_prefix='/wikis/')
 
 @bp.route('')
@@ -10,9 +14,12 @@ def index():
 
 @bp.route('uploadcsv', methods=(["POST"]))
 def upload():
-    print(request.files["csv"])
-    print(request.files["config"])
-    return "Uploaded"
+    config = csv2wiki.parse_config_string(request.files["config"].read().decode("utf-8"))
+    csv_in = csv2wiki.CSVInput(StringIO(request.files["csv"].read().decode('utf-8')), config)
+    output = StringIO()
+    wiki_sess = csv2wiki.WikiSession(config, csv_in, False, output)
+    wiki_sess.make_pages(None, "size")
+    return "<pre>" + output.getvalue() + "</pre>"
 
 # This section for standalone development/testing only
 if __name__ == "wikis":
