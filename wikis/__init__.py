df4fe9270b422e2702cc77fd212c0b4c398a1c44
Frank Duncan <frank@kank.net>
2018-11-02 05:35:45 -0500

    Add create wiki instance through UI
    This is largely a proof of concept commit that will be overwritten
    once there's a more formalized wiki addition system.
    
    This requires a script to be on your machine that can add a wiki
    to the farm, as in MEDIAWIKI_INSTALL.md, with the passwords
    and accounts that are included in that file.  It also forces you
    to have set up some domain "<wiki>.otswiki.net" as detailed in
    that file.

--- wikis/__init__.py
+++ wikis/__init__.py
@@ -1,32 +1,22 @@
 from flask import (
-    Blueprint, render_template, request
+    Blueprint, render_template
 )
 
-from io import StringIO
-
-import csv2wiki
-
 bp = Blueprint('wikis', __name__, url_prefix='/wikis/', template_folder='templates')
+
+from wikis import create
 
 @bp.route('')
 def index():
     return render_template("upload.html");
 
-@bp.route('uploadcsv', methods=(["POST"]))
-def upload():
-    config = csv2wiki.parse_config_string(request.files["config"].read().decode("utf-8"))
-    csv_in = csv2wiki.CSVInput(StringIO(request.files["csv"].read().decode('utf-8')), config)
-    output = StringIO()
-    wiki_sess = csv2wiki.WikiSession(config, csv_in, False, output)
-    wiki_sess.make_pages(None, "size")
-    return "<pre>" + output.getvalue() + "</pre>"
-
 def main_partial():
     return "main_partial.html"
 
 # This section for standalone development/testing only
-if __name__ == "wikis":
+if __name__ == "__main__" or __name__ == "wikis":
     from flask import Flask
     app = Flask(__name__)
     app.register_blueprint(bp)
-    app.run(host='0.0.0.0')
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0')
---
+++ wikis/create.py
@@ -0,0 +1,43 @@
+from io import StringIO
+from flask import (
+    request
+)
+
+import csv2wiki, subprocess
+
+from wikis import bp
+
+@bp.route('createwiki', methods=(["POST"]))
+def upload_test():
+    # Let this error if the script isn't here, since we're in prototype mode
+    # TODO: Replace by ansible call
+    subprocess.call(['addWiki.sh', request.form["name"]]);
+
+    return ("<a href='http://" +
+        request.form["name"] +
+        ".otswiki.net'>New wiki: " +
+        request.form["name"] + "</a>")
+
+@bp.route('uploadcsv', methods=(["POST"]))
+def upload():
+    # Let this error if the script isn't here, since we're in prototype mode
+    # TODO: Replace by ansible call
+    subprocess.call(['addWiki.sh', request.form["name"]]);
+
+    config = csv2wiki.parse_config_string(request.files["config"].read().decode("utf-8"))
+
+    # Override config options with our known parameters
+    config["wiki_url"] = "http://" + request.form["name"] + ".otswiki.net/"
+    # These are set in the addWiki.sh script, and should come from user federation later
+    config["username"] = "test"
+    config["password"] = "mdpwiki8chars"
+
+    csv_in = csv2wiki.CSVInput(StringIO(request.files["csv"].read().decode('utf-8')), config)
+    output = StringIO()
+    wiki_sess = csv2wiki.WikiSession(config, csv_in, False, output)
+    wiki_sess.make_pages(None, "size")
+    return ("<pre>" + output.getvalue() + "</pre>" +
+            "<a href='http://" +
+            request.form["name"] +
+            ".otswiki.net'>New wiki: " +
+            request.form["name"] + "</a>")
